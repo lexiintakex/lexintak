dc6e741fdeb1b7899f0d54374605e5037c167f94
Muhammad Yameen <muhammadyameen5432@gmail.com>
2025-08-03 21:23:14 +0500

    fix: fix that issues

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "https://lexiintake.com/api/api/",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://lexiintake.com/api/api/",
 });
 
 axiosInstance.interceptors.request.use(
